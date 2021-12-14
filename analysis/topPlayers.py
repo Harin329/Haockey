98c983f90e2ce13e8743783c4b2f4e7666e36df9
Harin <master.harin@gmail.com>
2021-12-14 00:36:28 -0800

    add date cycle

--- analysis/topPlayers.py
+++ analysis/topPlayers.py
@@ -2,6 +2,7 @@ import logging
 from logging import config
 from yahoo_oauth import OAuth2
 import yahoo_fantasy_api as yfa
+import time
 import datetime
 from datetime import timedelta
 import matplotlib.pyplot as plt
@@ -40,9 +41,10 @@ def calculateFantasyPoints(player):
     return totalPoint
 
 print("==================================Analysis====================================")
+playerItem = []
 playerID = []
 
-for num in range(10):
+for num in range(5):
     url = "https://fantasysports.yahooapis.com/fantasy/v2/league/411.l.96677/players;start=" + str(num * 25) + ";sort=PTS;count=25/percent_owned"
     print(url)
     response = sc.session.get(url, params={'format': 'json'})
@@ -50,60 +52,43 @@ for num in range(10):
 
     players = list(res['fantasy_content']['league'][1]['players'].values())
     for player in players[:-1]:
-        playerID.append({ "id": player['player'][0][1]['player_id'], "name": player['player'][0][2]['name']['full'] })
+        playerItem.append({ "id": player['player'][0][1]['player_id'], "name": player['player'][0][2]['name']['full'] })
+        playerID.append(player['player'][0][1]['player_id'])
 
 with open("players.txt", "w") as txt_file:
-    for line in playerID:
+    for line in playerItem:
         txt_file.write(str(line) + "\n")
 
-# playerMapping = {}
-
-# taken = league.taken_players()
-# taken = list(map(lambda x: x['player_id'], taken))
-# # print(taken)
-
-# faC = league.free_agents('C')
-# faC = list(map(lambda x: x['player_id'], faC))
-
-# faW = league.free_agents('W')
-# faW = list(map(lambda x: x['player_id'], faW))
-
-# faD = league.free_agents('D')
-# faD = list(map(lambda x: x['player_id'], faD))
-# # print(faD)
-
-# waivers = league.waivers()
-# waivers = list(map(lambda x: x['player_id'], waivers))
-# # print(waivers)
-
-# playerDB = taken + faC + faW + faD + waivers
-
-# start = datetime.datetime(2021, 10, 12)
-# end = datetime.datetime.now()
-
-# while start <= end:
-#     stats = league.player_stats(playerDB, 'date', date=start)
-#     for player in stats:
-#         name = str(player['player_id']) + "_" + player['name']
-#         # stats = list(map(lambda x: [x['name'], x['player_id'], calculateFantasyPoints(x)], stats))
-#         if name not in playerMapping:
-#             playerMapping[name] = [calculateFantasyPoints(player)]
-#         else:
-#             p = playerMapping[name]
-#             p.append(p[-1] + calculateFantasyPoints(player))
-#     start += timedelta(days=1)
-#     print(start)
+playerMapping = {}
 
 
-# config.dictConfig({
-#     'version': 1,
-#     'disable_existing_loggers': True,
-# })
+start = datetime.datetime(2021, 10, 12)
+end = datetime.datetime.now()
 
-# for key, value in playerMapping.items():
-#     plt.plot(value, label=key)
+while start <= end:
+    stats = league.player_stats(playerID, 'date', date=start)
+    # stats = list(map(lambda x: [x['name'], x['player_id'], calculateFantasyPoints(x)], stats))
+    for player in stats:
+        name = str(player['player_id']) + "_" + player['name']
+        if name not in playerMapping:
+            playerMapping[name] = [calculateFantasyPoints(player)]
+        else:
+            p = playerMapping[name]
+            p.append(p[-1] + calculateFantasyPoints(player))
+    start += timedelta(days=1)
+    print(start)
+    time.sleep(5)
 
-# plt.ylabel('Points')
-# plt.xlabel('Days')
-# plt.legend()
-# plt.show()
+
+config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+})
+
+for key, value in playerMapping.items():
+    plt.plot(value, label=key)
+
+plt.ylabel('Points')
+plt.xlabel('Days')
+plt.legend()
+plt.show()
