46fa3dcbcc03442b22b37848f28ea27c04051903
Harin <master.harin@gmail.com>
2021-12-16 00:17:06 -0800

    transfer to nhl api complete

--- analysis/getData.py
+++ analysis/getData.py
@@ -1,5 +1,6 @@
 import logging
 from logging import config
+import requests
 from yahoo_oauth import OAuth2
 import yahoo_fantasy_api as yfa
 import time
@@ -17,147 +18,210 @@ league = yfa.League(sc, '411.l.96677')
 team = yfa.Team(sc, '411.l.96677.t.2')
 
 definitions = {
-    'ID': 'ID',
-    'Name': 'Name',
-    'Position': 'Position',
-    'GP': 'Games Played',
-    'G': 'Goals',
-    'A': 'Assists',
-    'PTS': 'Points',
-    '+/-': 'Plus/Minus',
-    'PIM': 'Penalty Minutes',
-    'PPG': 'Power Play Goals',
-    'PPA': 'Power Play Assists',
-    'PPP': 'Power Play Points',
-    'SHG': 'Short Handed Goals',
-    'GWG': 'Game Winning Goals',
-    'SOG': 'Shots On Goal',
-    'S%': 'Shooting Percentage',
-    'HIT': 'Hits',
-    'BLK': 'Blocks',
-    'PPT': 'Power Play Time',
-    'Avg-PPT': 'Average Power Play Time',
-    'SHT': 'Short Handed Time',
-    'Avg-SHT': 'Average Short Handed Time',
-    'COR': 'Corsi',
-    'FEN': 'Fenwick',
-    'Off-ZS': 'Offensive Zone Starts',
-    'Def-ZS': 'Defense Zone Starts',
-    'ZS-Pct': 'Zone Start Percentage',
-    'GStr': 'Game Star',
-    'Shifts': 'Shifts',
-    'FP': 'Fan Points',
-    'GP_W': 'Games Played This Week',
-    'G_W': 'Goals This Week',
-    'A_W': 'Assists This Week',
-    'PTS_W': 'Points This Week',
-    '+/-_W': 'Plus/Minus This Week',
-    'PIM_W': 'Penalty Minutes This Week',
-    'PPG_W': 'Power Play Goals This Week',
-    'PPA_W': 'Power Play Assists This Week',
-    'PPP_W': 'Power Play Points This Week',
-    'SHG_W': 'Short Handed Goals This Week',
-    'GWG_W': 'Game Winning Goals This Week',
-    'SOG_W': 'Shots On Goal This Week',
-    'S%_W': 'Shooting Percentage This Week',
-    'HIT_W': 'Hits This Week',
-    'BLK_W': 'Blocks This Week',
-    'PPT_W': 'Power Play Time This Week',
-    'Avg-PPT_W': 'Average Power Play Time This Week',
-    'SHT_W': 'Short Handed Time This Week',
-    'Avg-SHT_W': 'Average Short Handed Time This Week',
-    'COR_W': 'Corsi This Week',
-    'FEN_W': 'Fenwick This Week',
-    'Off-ZS_W': 'Offensive Zone Starts This Week',
-    'Def-ZS_W': 'Defense Zone Starts This Week',
-    'ZS-Pct_W': 'Zone Start Percentage This Week',
-    'GStr_W': 'Game Star This Week',
-    'Shifts_W': 'Shifts This Week',
-    'FP_W': 'Fan Points This Week',
-    # 'Team': 'Team',
+    'ID': '8480800',
+    'Name': 'Quinn Hughes',
+    'Position': 'D',
+    'Team': 'VAN',
+    "timeOnIce" : "27:28",
+    "assists" : 3,
+    "goals" : 0,
+    "pim" : 0,
+    "shots" : 3,
+    "games" : 1,
+    "hits" : 0,
+    "powerPlayGoals" : 0,
+    "powerPlayPoints" : 1,
+    "powerPlayTimeOnIce" : "01:12",
+    "evenTimeOnIce" : "26:16",
+    "penaltyMinutes" : "0",
+    "faceOffPct" : 50.0,
+    "shotPct" : 0.0,
+    "gameWinningGoals" : 0,
+    "overTimeGoals" : 0,
+    "shortHandedGoals" : 0,
+    "shortHandedPoints" : 0,
+    "shortHandedTimeOnIce" : "00:00",
+    "blocked" : 0,
+    "plusMinus" : 0,
+    "points" : 3,
+    "shifts" : 32,
+    "fanPts" : 0,
+    'timeOnIce_W': '',
+    'assists_W': '',
+    'goals_W': '',
+    'pim_W': '',
+    'shots_W': '',
+    'games_W': '',
+    'hits_W': '',
+    'powerPlayGoals_W': '',
+    'powerPlayPoints_W': '',
+    'powerPlayTimeOnIce_W': '',
+    'evenTimeOnIce_W': '',
+    'penaltyMinutes_W': '',
+    'faceOffPct_W': '',
+    'shotPct_W': '',
+    'gameWinningGoals_W': '',
+    'overTimeGoals_W': '',
+    'shortHandedGoals_W': '',
+    'shortHandedPoints_W': '',
+    'shortHandedTimeOnIce_W': '',
+    'blocked_W': '',
+    'plusMinus_W': '',
+    'points_W': '',
+    'shifts_W': '',
+    'fanPts_W': '',
 }
 
 def calculateFantasyPoints(player):
-    if player['GP'] == '-' or player['position_type'] == 'G':
-        return 0
     totalPoint = 0
-    totalPoint += player['G'] * 6
-    totalPoint += player['A'] * 4
-    totalPoint += player['+/-'] * 2
-    totalPoint += player['PPG'] * 8
-    totalPoint += player['PPP'] * 2
-    totalPoint += player['SHG'] * 10
-    totalPoint += player['GWG'] * 10
-    totalPoint += player['SOG'] * 1
-    totalPoint += player['HIT'] * 1
-    totalPoint += player['BLK'] * 1
+    totalPoint += player['goals'] * 6
+    totalPoint += player['assists'] * 4
+    totalPoint += player['plusMinus'] * 2
+    totalPoint += player['powerPlayGoals'] * 8
+    totalPoint += player['powerPlayPoints'] * 2
+    totalPoint += player['shortHandedGoals'] * 10
+    totalPoint += player['gameWinningGoals'] * 10
+    totalPoint += player['shots'] * 1
+    totalPoint += player['hits'] * 1
+    totalPoint += player['blocked'] * 1
     return totalPoint
+
+# Convert Time String to Seconds
+def convertTimeToSeconds(timeString):
+    timeList = timeString.split(':')
+    return int(timeList[0]) * 60 + int(timeList[1])
+
+def addGame(current, newGameMap):
+    for key in newGameMap:
+        if key in ['timeOnIce', 'powerPlayTimeOnIce', 'evenTimeOnIce', 'shortHandedTimeOnIce']:
+            current[key] = current[key] + convertTimeToSeconds(newGameMap[key])
+        elif key in ['shotPct', 'faceOffPct']:
+            current[key] = (current[key] + newGameMap[key]) / 2 
+        else:
+            current[key] = current[key] + int(newGameMap[key])
+    current['fanPts'] = current['fanPts'] + calculateFantasyPoints(newGameMap)
+    return current
 
 print("==================================Get Data====================================")
 # [Stats @ Week X] --> [PPG @ Week X + 1 ]
 
-# Get top 250 NHL players
+# Get active NHL players
 # playerItem = []
 
-# for page in range(10):
-#     url = "https://fantasysports.yahooapis.com/fantasy/v2/league/411.l.96677/players;start=" + str(page * 25) + ";sort=PTS;count=25"
-#     response = sc.session.get(url, params={'format': 'json'})
-#     res = response.json()
+# response = requests.get('https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster')
+# res = response.json()
 
-#     players = list(res['fantasy_content']['league'][1]['players'].values())
-#     for player in players[:-1]:
-#         playerItem.append([player['player'][0][1]['player_id'], player['player'][0][2]['name']['full']])
+# teams = res['teams']
+# for team in teams:
+#     print(team['name'])
+#     roster = team['roster']['roster']
+#     for player in roster:
+#         print(player['person']['fullName'])
+#         playerItem.append([player['person']['id'], player['person']['fullName'], player['position']['code'], team['abbreviation']])
 
 # with open("players.csv", "w") as f:
 #     writer = csv.writer(f)
 #     for player in playerItem:
 #         writer.writerow(player)
-# print("Top 250 players saved to players.csv!")
+# print("Active players saved to players.csv!")
+
 
 
 # For each player, extract format in form of sample data above
+df = pd.DataFrame(columns=definitions.keys())
 playerID = []
 startDate = datetime.datetime(2021, 10, 12)
 
 with open('players.csv', mode ='r') as playersFile:
-  reader = csv.reader(playersFile)
-  for player in reader:
-        playerID.append(player[0])
+    reader = csv.reader(playersFile)
+    for player in reader:
+        if player[2] != 'G':
+            playerID.append(player[0])
+            response = requests.get('https://statsapi.web.nhl.com/api/v1/people/' + player[0] + '/stats?stats=gameLog&season=20212022')
+            res = response.json()
 
-# {'player_id': 7904, 'name': 'Filip Zadina', 'position_type': 'P', 'GP': 29.0, 'G': 4.0, 'A': 6.0, 'PTS': 10.0, '+/-': -7.0, 'PIM': 4.0, 'PPG': 2.0, 'PPA': 2.0, 'PPP': 4.0, 'SHG': 0.0, 'GWG': 1.0, 'SOG': 70.0, 'S%': 0.057, 'HIT': 19.0, 'BLK': 17.0, 'PPT': 3389.0, 'Avg-PPT': 117.0, 'SHT': 3.0, 'Avg-SHT': 0.0, 'COR': 3.0, 'FEN': 14.0, 'Off-ZS': 138.0, 'Def-ZS': 98.0, 'ZS-Pct': 58.47, 'GStr': 0.0, 'Shifts': 498.0}
-# {'player_id': 7904, 'name': 'Filip Zadina', 'position_type': 'P', 'GP': '-', 'G': '-', 'A': '-', 'PTS': '-', '+/-': '-', 'PIM': '-', 'PPG': '-', 'PPA': '-', 'PPP': '-', 'SHG': '-', 'GWG': '-', 'SOG': '-', 'S%': '-', 'HIT': '-', 'BLK': '-'}
+            playerInfo = [player[0], player[1], player[2], player[3]]
 
-df = pd.DataFrame(columns=definitions.keys())
+            sunday = datetime.datetime(2021, 10, 17)
+            
+            weekInfo = {
+                "timeOnIce" : 0,
+                "assists" : 0,
+                "goals" : 0,
+                "pim" : 0,
+                "shots" : 0,
+                "games" : 0,
+                "hits" : 0,
+                "powerPlayGoals" : 0,
+                "powerPlayPoints" : 0,
+                "powerPlayTimeOnIce" : 0,
+                "evenTimeOnIce" : 0,
+                "penaltyMinutes" : 0,
+                "faceOffPct" : 0,
+                "shotPct" : 0,
+                "gameWinningGoals" : 0,
+                "overTimeGoals" : 0,
+                "shortHandedGoals" : 0,
+                "shortHandedPoints" : 0,
+                "shortHandedTimeOnIce" : 0,
+                "blocked" : 0,
+                "plusMinus" : 0,
+                "points" : 0,
+                "shifts" : 0,
+                "fanPts" : 0,
+            }
+            seasonInfo = weekInfo.copy()
+            resultPPG = 0
 
-stats = league.player_stats(playerID, 'season')
-weeklyStats = league.player_stats(playerID, 'date', startDate)
-print(weeklyStats)
+            gameList = list(res['stats'][0]['splits'])
+            gameList.reverse()
 
-# for index, id in enumerate(playerID):
-#     player = stats[index]
-#     weeklyPlayer = weeklyStats[index]
-#     if (player['position_type'] != 'G'):
-#         seasonList = list(player.values())
-#         seasonFan = [calculateFantasyPoints(player)]
-#         weekList = list(weeklyPlayer.values())[3:]
-#         weekFan = [calculateFantasyPoints(weeklyPlayer)]
-#         finalList = seasonList + seasonFan + weekList + weekFan
-#         print(len(finalList))
-#         print(len(seasonFan))
-#         print(len(weekList))
-#         print(len(weekFan))
-#         df.loc[id, :] = finalList
-# print(df)
+            for game in gameList:
+                if (datetime.datetime.strptime(game['date'], '%Y-%m-%d')) <= sunday:
+                    weekInfo = addGame(weekInfo, game['stat'])
+                    seasonInfo = addGame(seasonInfo, game['stat'])
+                    resultPPG += game['stat']['powerPlayGoals']
+                else:
+                    print('Write to Week')
+                    row = playerInfo + list(seasonInfo.values()) + list(weekInfo.values())
+                    df.loc[len(df)] = row
+                    # print(row)
+                    # print(resultPPG)
+                    
+                    sunday += datetime.timedelta(days=7)
+                    resultPPG = 0
+                    weekInfo = {
+                        "timeOnIce" : 0,
+                        "assists" : 0,
+                        "goals" : 0,
+                        "pim" : 0,
+                        "shots" : 0,
+                        "games" : 0,
+                        "hits" : 0,
+                        "powerPlayGoals" : 0,
+                        "powerPlayPoints" : 0,
+                        "powerPlayTimeOnIce" : 0,
+                        "evenTimeOnIce" : 0,
+                        "penaltyMinutes" : 0,
+                        "faceOffPct" : 0,
+                        "shotPct" : 0,
+                        "gameWinningGoals" : 0,
+                        "overTimeGoals" : 0,
+                        "shortHandedGoals" : 0,
+                        "shortHandedPoints" : 0,
+                        "shortHandedTimeOnIce" : 0,
+                        "blocked" : 0,
+                        "plusMinus" : 0,
+                        "points" : 0,
+                        "shifts" : 0,
+                        "fanPts" : 0,
+                    }
+                    weekInfo = addGame(weekInfo, game['stat'])
+                    seasonInfo = addGame(seasonInfo, game['stat'])
+                    resultPPG += game['stat']['powerPlayGoals']
 
-# resultList = []
-    
-# start = datetime.datetime(2021, 10, 12)
-# sunday = datetime.datetime(2021, 10, 17)
-# end = datetime.datetime.now()
 
-# while start <= end:
-#     start += timedelta(days=1)
-#     print(start)
+print(df.head(100))
 
 
 # Append processed data into csv file
