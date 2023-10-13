eb34e57878b09c983d4db8d37c7f1682301d17e8
Harin <master.harin@gmail.com>
2023-10-12 21:05:37 -0700

    Update for 2023 Season

--- fantasy/main.py
+++ fantasy/main.py
@@ -2,7 +2,16 @@ from yahoo_oauth import OAuth2
 import yahoo_fantasy_api as yfa
 import schedule
 import time
+import boto3
+from dotenv import dotenv_values
+
+config = dotenv_values("../auth/.env")
+
 sc = OAuth2(None, None, from_file='../auth/auth.json')
+session = boto3.Session(
+    aws_access_key_id=config["AWS_ACCESS_KEY"],
+    aws_secret_access_key=config["AWS_SECRET_KEY"],
+)
 
 if not sc.token_is_valid():
     print("Refreshing Token...")
@@ -10,17 +19,36 @@ if not sc.token_is_valid():
 
 # Get League ID
 gm = yfa.Game(sc, 'nhl')
-print("League ID: " + str(gm.league_ids(year=2021)))
+print("League ID: " + str(gm.league_ids(year=2023)))
 
-league = yfa.League(sc, '411.l.96677')
+league = yfa.League(sc, '427.l.43850')
 print("League Standing: " + str(league.standings()))
 
-team = yfa.Team(sc, '411.l.96677.t.2')
+team = yfa.Team(sc, '427.l.43850.t.1')
 print("Team Roster: " + str(team.roster()))
+
+dynamodb = session.resource('dynamodb')
+table = dynamodb.Table('Fantasy')
 
 ## Weekly Pickup (Add, Drop)
 def pickup():
     print("Weekly Pickup...")
+    # List all partition keys in table
+    response = table.scan()
+    # get partition key for each player
+    for player in response['Items']:
+        dropID = player['DropID']
+        addID = player['AddID']
+        try:
+            team.add_and_drop_players(addID, dropID)
+            print("Dropped " + str(dropID) + " and added " + str(addID))
+        except:
+            print("Failed to drop " + str(dropID) + " and add " + str(addID))
+
+
+## Manual Weekly Pickup (Add, Drop)
+def manualPickup():
+    print("Manual Weekly Pickup...")
     # team.add_and_drop_players(6571, 5366)
     # team.add_and_drop_players(3349, 5391)
     # team.add_and_drop_players(6002, 8287)
